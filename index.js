0be190cabb77d3a226df1986ee84fe3ade80992d
fakhrul62 <ifakhrul23@gmail.com>
2024-12-06 19:36:44 +0600

    user/product function  added

--- index.js
+++ index.js
@@ -27,12 +27,88 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // Connect to the "storeDB" database and access its "store" collection
+    const storeCollection = client.db("storeDB").collection("store");
+    const userCollection = client.db("storeDB").collection("users");
+
+    // posting it to the server
+    app.post("/store", async (req, res) => {
+      const newProduct = req.body;
+      console.log(newProduct);
+      const result = await storeCollection.insertOne(newProduct);
+      res.send(result);
+    });
+    //getting data from the server
+    app.get("/store", async (req, res) => {
+      const cursor = storeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+
+        /////==============================================USER RELATED APIs=========================================================
+    //create new user
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      console.log("creating new user: ", newUser);
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
+    // getting users
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    //get a data in the server
+    app.get("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+    //update a data in the server
+      app.put("/user/:id", async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const updateUser = req.body;
+        const user = {
+          $set: {
+            email: updateUser.email,
+            password: updateUser.password,
+          },
+        };
+        const result = await userCollection.updateOne(filter, user, options);
+        res.send(result);
+      });
+    //delete a data from the server
+    app.delete("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+    //patch
+    app.patch("/user", async (req, res) => {
+      const email = req.body.email;
+      const filter = {email};
+      const updateDoc = {
+        $set: {
+          lastSignInTime: req?.body?.lastSignInTime
+        }
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log("Pinged your deployment. You successfully connected to StoreDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
